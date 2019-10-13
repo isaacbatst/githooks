ae054c5da652c51a3e596be7d7c37923e178225b
Isaac Batista <isaacbatst@gmail.com>
2019-10-13 14:05:07 -0300

    List and Toogle my favorite repos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <h1>Hello hooks</h1>;
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.github.com/users/isaacbatst/repos');
+      const data = await response.json();
+      setRepositories(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(()=> {
+    const favoriteRepos = repositories.filter(repo => repo.favorite);
+
+    document.title = `${favoriteRepos.length} favorites`
+  },[repositories])
+
+  function handleFavorite(id){
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories);
+  };
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite ? <span>(Favorite)</span> : ""}
+            <button onClick={() => handleFavorite(repo.id) }>Star</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
